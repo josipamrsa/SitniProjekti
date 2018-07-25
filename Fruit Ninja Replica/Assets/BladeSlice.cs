40e99f262de0f5e17adb83d31937c0261af7498c
Josipa Mrsa <jmrsa21@gmail.com>
2018-07-25 12:15:54 +0200

    Unity3D - Fruit Ninja (samo osnovno)

---
+++ Fruit Ninja Replica/Assets/BladeSlice.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeSlice : MonoBehaviour {
+
+    bool isCutting = false;
+    Rigidbody2D rb;
+    Camera cam;
+    public GameObject bladeTrailPrefab;
+    GameObject currentBladeTrail;
+    CircleCollider2D c2d;
+    Vector2 prevPosition;
+    public float minCuttingVelocity = 0.001f;
+    void Start()
+    {
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+        c2d = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCutting();
+        }
+
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopCutting();
+        }
+
+        if (isCutting)
+        {
+            UpdateCut();
+        }
+    }
+
+    void StartCutting()
+    {
+        isCutting = true;
+        currentBladeTrail = Instantiate(bladeTrailPrefab, transform);
+        prevPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        c2d.enabled = false;
+    }
+    void StopCutting()
+    {
+        isCutting = false;
+        currentBladeTrail.transform.SetParent(null);
+        Destroy(currentBladeTrail, 2f);
+        c2d.enabled = false;
+    }
+
+    void UpdateCut()
+    {        
+       
+        Vector2 newPosition=cam.ScreenToWorldPoint(Input.mousePosition);
+        rb.position = newPosition;
+
+        float velocity = (newPosition - prevPosition).magnitude * Time.deltaTime;
+        if (velocity>minCuttingVelocity)
+        {
+            c2d.enabled = true;
+        }
+
+        else
+        {
+            c2d.enabled = false;
+        }
+
+        prevPosition = newPosition;
+
+    }
+}
---
+++ Fruit Ninja Replica/Assets/Fruit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour {
+
+    public GameObject fruitSlicedPrefab;
+    Rigidbody2D rb;
+    public float startForce = 15f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * startForce, ForceMode2D.Impulse);
+    }
+
+   
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Blade")
+        {
+            Vector3 dir = (col.transform.position - transform.position).normalized;
+            Quaternion rotation = Quaternion.LookRotation(dir);
+            GameObject slicedFruit = Instantiate(fruitSlicedPrefab, transform.position, rotation);
+            Destroy(slicedFruit, 3f);
+            Destroy(gameObject);
+        }
+    }
+
+}
